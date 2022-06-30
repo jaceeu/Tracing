844ac1a7649fdea848f1d85daecf4656423fa949
Jhan Carlo Vasquez <jhancarlo.vasquez@tup.edu.ph>
2022-06-30 16:49:37 +0800

    first and last commit

---
+++ Tracing/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Tracing
+{
+    partial class DaTrace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnInfo1 = new System.Windows.Forms.Button();
+            this.btnInfo2 = new System.Windows.Forms.Button();
+            this.btnInfo3 = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox3 = new System.Windows.Forms.TextBox();
+            this.txtBox4 = new System.Windows.Forms.TextBox();
+            this.txtBox5 = new System.Windows.Forms.TextBox();
+            this.txtBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.InfotxtBox1 = new System.Windows.Forms.RichTextBox();
+            this.InfotxtBox2 = new System.Windows.Forms.RichTextBox();
+            this.InfotxtBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(156, 227);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Save";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(156, 256);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Save";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(156, 285);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Save";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btnInfo1
+            // 
+            this.btnInfo1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnInfo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInfo1.Location = new System.Drawing.Point(237, 227);
+            this.btnInfo1.Name = "btnInfo1";
+            this.btnInfo1.Size = new System.Drawing.Size(75, 23);
+            this.btnInfo1.TabIndex = 3;
+            this.btnInfo1.Text = "Info";
+            this.btnInfo1.UseVisualStyleBackColor = false;
+            this.btnInfo1.Click += new System.EventHandler(this.btnInfo1_Click);
+            // 
+            // btnInfo2
+            // 
+            this.btnInfo2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnInfo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInfo2.Location = new System.Drawing.Point(237, 256);
+            this.btnInfo2.Name = "btnInfo2";
+            this.btnInfo2.Size = new System.Drawing.Size(75, 23);
+            this.btnInfo2.TabIndex = 4;
+            this.btnInfo2.Text = "Info";
+            this.btnInfo2.UseVisualStyleBackColor = false;
+            this.btnInfo2.Click += new System.EventHandler(this.btnInfo2_Click);
+            // 
+            // btnInfo3
+            // 
+            this.btnInfo3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnInfo3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInfo3.Location = new System.Drawing.Point(237, 285);
+            this.btnInfo3.Name = "btnInfo3";
+            this.btnInfo3.Size = new System.Drawing.Size(75, 23);
+            this.btnInfo3.TabIndex = 5;
+            this.btnInfo3.Text = "Info";
+            this.btnInfo3.UseVisualStyleBackColor = false;
+            this.btnInfo3.Click += new System.EventHandler(this.btnInfo3_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(158, 74);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(154, 20);
+            this.txtBox1.TabIndex = 6;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(158, 100);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(154, 20);
+            this.txtBox2.TabIndex = 7;
+            // 
+            // txtBox3
+            // 
+            this.txtBox3.Location = new System.Drawing.Point(158, 126);
+            this.txtBox3.Name = "txtBox3";
+            this.txtBox3.Size = new System.Drawing.Size(154, 20);
+            this.txtBox3.TabIndex = 8;
+            // 
+            // txtBox4
+            // 
+            this.txtBox4.Location = new System.Drawing.Point(158, 152);
+            this.txtBox4.Name = "txtBox4";
+            this.txtBox4.Size = new System.Drawing.Size(154, 20);
+            this.txtBox4.TabIndex = 9;
+            // 
+            // txtBox5
+            // 
+            this.txtBox5.Location = new System.Drawing.Point(158, 175);
+            this.txtBox5.Name = "txtBox5";
+            this.txtBox5.Size = new System.Drawing.Size(154, 20);
+            this.txtBox5.TabIndex = 10;
+            // 
+            // txtBox6
+            // 
+            this.txtBox6.Location = new System.Drawing.Point(158, 201);
+            this.txtBox6.Name = "txtBox6";
+            this.txtBox6.Size = new System.Drawing.Size(154, 20);
+            this.txtBox6.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(102, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Contact No.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(94, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Time In";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(83, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Time Out";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(110, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Date";
+            // 
+            // InfotxtBox1
+            // 
+            this.InfotxtBox1.Location = new System.Drawing.Point(336, 75);
+            this.InfotxtBox1.Name = "InfotxtBox1";
+            this.InfotxtBox1.Size = new System.Drawing.Size(100, 152);
+            this.InfotxtBox1.TabIndex = 18;
+            this.InfotxtBox1.Text = "";
+            this.InfotxtBox1.TextChanged += new System.EventHandler(this.InfotxtBox1_TextChanged);
+            // 
+            // InfotxtBox2
+            // 
+            this.InfotxtBox2.Location = new System.Drawing.Point(442, 75);
+            this.InfotxtBox2.Name = "InfotxtBox2";
+            this.InfotxtBox2.Size = new System.Drawing.Size(100, 152);
+            this.InfotxtBox2.TabIndex = 19;
+            this.InfotxtBox2.Text = "";
+            this.InfotxtBox2.TextChanged += new System.EventHandler(this.InfotxtBox2_TextChanged);
+            // 
+            // InfotxtBox3
+            // 
+            this.InfotxtBox3.Location = new System.Drawing.Point(548, 75);
+            this.InfotxtBox3.Name = "InfotxtBox3";
+            this.InfotxtBox3.Size = new System.Drawing.Size(100, 152);
+            this.InfotxtBox3.TabIndex = 20;
+            this.InfotxtBox3.Text = "";
+            this.InfotxtBox3.TextChanged += new System.EventHandler(this.InfotxtBox3_TextChanged);
+            // 
+            // DaTrace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.InfotxtBox3);
+            this.Controls.Add(this.InfotxtBox2);
+            this.Controls.Add(this.InfotxtBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox6);
+            this.Controls.Add(this.txtBox5);
+            this.Controls.Add(this.txtBox4);
+            this.Controls.Add(this.txtBox3);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.btnInfo3);
+            this.Controls.Add(this.btnInfo2);
+            this.Controls.Add(this.btnInfo1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "DaTrace";
+            this.Text = "DaTrace";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnInfo1;
+        private System.Windows.Forms.Button btnInfo2;
+        private System.Windows.Forms.Button btnInfo3;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox3;
+        private System.Windows.Forms.TextBox txtBox4;
+        private System.Windows.Forms.TextBox txtBox5;
+        private System.Windows.Forms.TextBox txtBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox InfotxtBox1;
+        private System.Windows.Forms.RichTextBox InfotxtBox2;
+        private System.Windows.Forms.RichTextBox InfotxtBox3;
+    }
+}
+
---
+++ Tracing/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tracing
+{
+    public partial class DaTrace : Form
+    {
+        public DaTrace()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //StreamWriter File =File.AppendText(@"Downloads\test.txt");
+
+            StreamWriter file = new StreamWriter(@"C:\Users\Carlo\source\test1.txt");
+            file.WriteLine("Name :" + txtBox1.Text);
+            file.WriteLine("Address :" + txtBox2.Text);
+            file.WriteLine("Contact No. :" + txtBox3.Text);
+            file.WriteLine("Time In. :" + txtBox4.Text);
+            file.WriteLine("Time Out. :" + txtBox5.Text);
+            file.WriteLine("Date. :" + txtBox6.Text);
+            file.Close();
+            MessageBox.Show("Thank you");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            StreamWriter file = new StreamWriter(@"C:\Users\Carlo\source\test2.txt");
+            file.WriteLine("Name :" + txtBox1.Text);
+            file.WriteLine("Address :" + txtBox2.Text);
+            file.WriteLine("Contact No. :" + txtBox3.Text);
+            file.WriteLine("Time In. :" + txtBox4.Text);
+            file.WriteLine("Time Out. :" + txtBox5.Text);
+            file.WriteLine("Date. :" + txtBox6.Text);
+            file.Close();
+            MessageBox.Show("Thank you");
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            StreamWriter file = new StreamWriter(@"C:\Users\Carlo\source\test3.txt");
+            file.WriteLine("Name :" + txtBox1.Text);
+            file.WriteLine("Address :" + txtBox2.Text);
+            file.WriteLine("Contact No. :" + txtBox3.Text);
+            file.WriteLine("Time In. :" + txtBox4.Text);
+            file.WriteLine("Time Out. :" + txtBox5.Text);
+            file.WriteLine("Date. :" + txtBox6.Text);
+            file.Close();
+            MessageBox.Show("Thank you");
+        }
+
+        private void btnInfo1_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile = new StreamReader(@"C:\Users\Carlo\source\test1.txt");
+            InfotxtBox1.Text = inputFile.ReadToEnd();
+            inputFile.Close();
+        }
+
+        private void btnInfo2_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile = new StreamReader(@"C:\Users\Carlo\source\test2.txt");
+            InfotxtBox2.Text = inputFile.ReadToEnd();
+            inputFile.Close();
+        }
+
+        private void btnInfo3_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile = new StreamReader(@"C:\Users\Carlo\source\test3.txt");
+            InfotxtBox3.Text = inputFile.ReadToEnd();
+            inputFile.Close();
+        }
+
+        private void InfotxtBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InfotxtBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InfotxtBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
